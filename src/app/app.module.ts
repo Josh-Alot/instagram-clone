97a6b3c5bb32bfcf9f1c1634aa7c7f62630b583c
Lucas Mendes <lucas.mendessantos93@gmail.com>
2021-08-10 19:34:29 -0300

    feat: creating components for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccessComponent } from './components/access/access.component';
+import { BannerComponent } from './components/access/banner/banner.component';
+import { LoginComponent } from './components/access/login/login.component';
+import { RegisterComponent } from './components/access/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    BannerComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
