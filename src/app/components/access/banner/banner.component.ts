5e3b375e51bf37b2e86a0a7d71d7732f2ad44ecb
Lucas Mendes <lucas.mendessantos93@gmail.com>
2021-08-11 12:45:22 -0300

    feat: creating the trigger, states and transitions on the banner component animations

--- src/app/components/access/banner/banner.component.ts
+++ src/app/components/access/banner/banner.component.ts
@@ -1,15 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'insta-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('banner', [ 
+      state('hidden', style({ 
+        opacity: 0,
+      })),
+      state('visible', style({
+        opacity: 1,
+      })),
+      transition('hidden <=> visible', animate('1s ease')),
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+  public bannerState = 'visible';
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  public toggleBannerState(): void {
+    this.bannerState = this.bannerState === 'hidden' ? 'visible' : 'hidden'
   }
 
 }
