0d1e41fb2df14d9d21f3f95b79a1da4a863d99cb
Lucas Mendes <lucas.mendessantos93@gmail.com>
2021-08-11 20:10:32 -0300

    feat: implementing keyframes and animation triggers on access component

--- src/app/components/access/access.component.ts
+++ src/app/components/access/access.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -27,8 +27,18 @@ import { Component, OnInit } from '@angular/core';
           opacity: 0,
           transform: 'translate(50px, 0)'
         }),
-        animate('500ms ease-in-out'),
-      ])
+        animate('1500ms 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 1, opacity: 1, transform: 'translateY(0)' }),
+        ])),
+      ]),
     ]),
   ]
 })
@@ -47,4 +57,11 @@ export class AccessComponent implements OnInit {
     this.registry = event === 'registry' ? true : false;
   }
 
+  public startAnimation(): void {
+    console.log('start animation');
+  }
+
+  public endAnimation(): void {
+    console.log('end animation');
+  }
 }
