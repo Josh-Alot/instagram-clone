61f47c6a5543a2d5dcb507aa54b29208e2894838
Lucas Mendes <lucas.mendessantos93@gmail.com>
2021-08-11 19:05:05 -0300

    feat: implementing the trigger animations for the banner container and the panel container on access component

--- src/app/components/access/access.component.ts
+++ src/app/components/access/access.component.ts
@@ -1,11 +1,40 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'insta-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.scss']
+  styleUrls: ['./access.component.scss'],
+  animations: [
+    trigger('banner-animation', [
+      state('created', style({
+        opacity: 1,
+      })),
+      transition('void => created', [
+        style({ 
+          opacity: 0,
+          transform: 'translate(-50px, 0)'
+         }),
+        animate('500ms ease-in-out'),
+      ]),
+    ]),
+    trigger('panel-animation', [
+      state('created', style({
+        opacity: 1,
+      })),
+      transition('void => created', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px, 0)'
+        }),
+        animate('500ms ease-in-out'),
+      ])
+    ]),
+  ]
 })
 export class AccessComponent implements OnInit {
+  public bannerState = 'created';
+  public panelState = 'created';
 
   constructor() { }
 
