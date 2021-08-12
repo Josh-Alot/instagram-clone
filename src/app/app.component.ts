8bc7b1a2a9dd579b3b8081602e69e9ca02bc2fbc
Lucas Mendes <lucas.mendessantos93@gmail.com>
2021-08-12 12:55:07 -0300

    feat: installing firebase sdk and implementing firebase on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import firebase from 'firebase';
 
 @Component({
   selector: 'insta-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'instagram-clone';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCRQo7kdGjwWyXIBdIPWiQa_X1YgEHRX-4",
+      authDomain: "instagram-clone-e1fc0.firebaseapp.com",
+      projectId: "instagram-clone-e1fc0",
+      storageBucket: "instagram-clone-e1fc0.appspot.com",
+      messagingSenderId: "652791676858",
+      appId: "1:652791676858:web:24ec392118ec6e64092783"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
